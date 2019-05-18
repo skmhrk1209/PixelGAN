f4683931ea181810e703640b247df546bfbcf6c5
Hiroki Sakuma <hirokisakuma@HirokinoMacBook-puro.local>
2019-05-19 03:13:39 +0900

    first commit

--- main.py
+++ main.py
@@ -112,7 +112,7 @@ def main():
 
     summary_writer = SummaryWriter(config.event_directory)
 
-    if args.training:
+    if config.training:
 
         os.makedirs(config.checkpoint_directory, exist_ok=True)
         os.makedirs(config.event_directory, exist_ok=True)
