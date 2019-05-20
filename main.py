b89cc0fbb25c86f9b16c883857e5f9afe2fb30ef
sakuma <sakuma@sakumanoMacBook-Pro.local>
2019-05-20 19:22:08 +0900

    update

--- main.py
+++ main.py
@@ -123,7 +123,7 @@ def main():
             download=True,
             transform=transforms.Compose([
                 transforms.ToTensor(),
-                transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+                transforms.Normalize((0.5,), (0.5,))
             ])
         )
 
